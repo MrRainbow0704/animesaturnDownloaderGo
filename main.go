91deec26d6b77074d6ccc2b35c3d695addb7faf8
MrRainbow0704 <2007.marcosimone@gmail.com>
2024-09-04 03:02:12 +0200

    Added command flags

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -161,50 +162,102 @@ func Downloader(id int, c *http.Client, path string, filename string, jobs <-cha
 	}
 }
 
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
+}
+
+func noFlags(names ...string) bool {
+	ret := true
+	for _, name := range names {
+		if isFlagPassed(name) {
+			ret = false
+		}
+	}
+	return ret
+}
+
 func main() {
-	// initializing reader
-	reader := bufio.NewReader(os.Stdin)
+	cwd, _ := os.Getwd()
 
-	// Getting the page link
-	fmt.Print("Inserisci il link alla pagina dell'anime: ")
-	link, _ := reader.ReadString('\n')
-	link = strings.TrimSpace(link)
+	// initialize flags arguments
+	const usage = `Flag di AnimesaturnDownloader:
+  -h, --help			prints help information
+  -u, --url, --link		link alla pagina dell'anime
+  -f, --first			primo episodio da scaricare
+  -l, --last			ultimo episodio da scaricare
+  -d, --dir, --path		percorso dove salvare i file
+  -n, --filename		nome del file, senza numero di episodio e estensione
+`
 
-	// Getting the first and last episodes to download
-	fmt.Print("Inserisci il primo episodio da scaricare: ")
-	primoStr, _ := reader.ReadString('\n')
+	var link string
+	flag.StringVar(&link, "link", "D", "link alla pagina dell'anime")
+	flag.StringVar(&link, "url", "D", "link alla pagina dell'anime")
+	flag.StringVar(&link, "u", "D", "link alla pagina dell'anime")
 	var primo int
-	if i, err := strconv.Atoi(primoStr); err == nil {
-		primo = i
-	} else {
-		primo = 0
-	}
-	fmt.Print("Inserisci l'ultimo episodio da scaricare: ")
-	ultimoStr, _ := reader.ReadString('\n')
+	flag.IntVar(&primo, "first", 0, "primo episodio da scaricare")
+	flag.IntVar(&primo, "f", 0, "primo episodio da scaricare")
 	var ultimo int
-	if i, err := strconv.Atoi(ultimoStr); err == nil {
-		ultimo = i
-	} else {
-		ultimo = -1
+	flag.IntVar(&ultimo, "last", -1, "ultimo episodio da scaricare")
+	flag.IntVar(&ultimo, "l", -1, "ultimo episodio da scaricare")
+	var path string
+	flag.StringVar(&path, "path", cwd, "percorso dove salvare i file")
+	flag.StringVar(&path, "dir", cwd, "percorso dove salvare i file")
+	flag.StringVar(&path, "d", cwd, "percorso dove salvare i file")
+	var filename string
+	flag.StringVar(&filename, "filename", "D", "nome del file, senza numero di episodio e estensione")
+	flag.StringVar(&filename, "n", "D", "nome del file, senza numero di episodio e estensione")
+
+	flag.Usage = func() { log.Print(usage) }
+	flag.Parse()
+
+	if noFlags("link", "primo", "ultimo", "path", "filename") {
+		// initializing reader
+		reader := bufio.NewReader(os.Stdin)
+
+		// Getting the page link
+		fmt.Print("Inserisci il link alla pagina dell'anime: ")
+		link, _ = reader.ReadString('\n')
+
+		// Getting the first and last episodes to download
+		fmt.Print("Inserisci il primo episodio da scaricare: ")
+		primoStr, _ := reader.ReadString('\n')
+		if i, err := strconv.Atoi(primoStr); err == nil {
+			primo = i
+		} else {
+			primo = 0
+		}
+		fmt.Print("Inserisci l'ultimo episodio da scaricare: ")
+		ultimoStr, _ := reader.ReadString('\n')
+		if i, err := strconv.Atoi(ultimoStr); err == nil {
+			ultimo = i
+		} else {
+			ultimo = -1
+		}
+
+		// Getting the path where to store the downloads
+		fmt.Printf("Inserisci il percorso dove salvare i file [Vuoto per: \"%s\"]: ", path)
+		path, _ = reader.ReadString('\n')
+
+		// Getting filenames
+		fmt.Print("Inserisci il nome per i file: ")
+		filename, _ = reader.ReadString('\n')
 	}
 
-	// Getting the path where to store the downloads
-	path, _ := os.Getwd()
-	fmt.Printf("Inserisci il percorso dove salvare i file [Vuoto per: \"%s\"]: ", path)
-	pathNew, _ := reader.ReadString('\n')
-	pathNew = strings.TrimSpace(pathNew)
-	if filepath.IsAbs(pathNew) {
-		path = pathNew
-	} else {
-		path = filepath.Join(path, pathNew)
+	// Input formatting
+	link = strings.TrimSpace(link)
+	path = strings.TrimSpace(path)
+	if !filepath.IsAbs(path) {
+		path = filepath.Join(cwd, path)
 	}
 	if err := os.MkdirAll(path, 0777); err != nil {
-		log.Panicf("Errore durante la creazione della directory: %s", err)
+		log.Panicf("Errore durante la creazione della directory `%s`: %s", path, err)
 	}
-
-	// Getting filenames
-	fmt.Print("Inserisci il nome per i file: ")
-	filename, _ := reader.ReadString('\n')
 	filename = strings.TrimSpace(filename)
 
 	var startTime = time.Now()
@@ -223,6 +276,9 @@ func main() {
 	}
 	if bytes.Equal(episodi[0], []byte("NOT FOUND")) && primo == 0 {
 		primo = 1
+	}
+	if ultimo == -1 {
+		ultimo = len(episodi) - 1
 	}
 	log.Print("Link agli episodi trovati!")
 
